682a362a1fd3bb7239501180c5a5ca5ea13e4782
teddy555 <1046429367@qq.com>
2017-03-13 10:30:03 +0800

    sy

--- sun/jiaoben.bat
+++
@@ -1,6 +0,0 @@
-git init
-git add sun
-git commit -m 'sun'
-git remote rm origin
-git remote add origin git@github.com:teddy555/teddy1.git
-git push -u origin master
--- sy/jiaoben.bat
+++
@@ -1,6 +0,0 @@
-git init
-git add sy
-git commit -m 'sy'
-git remote rm origin
-git remote add origin git@github.com:teddy555/teddy1.git
-git push -u origin master
